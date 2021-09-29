4473df6adea3cdc7fb36871f5585ca0ef76f190c
Thomas Kainz <kaithd17@htl-kaindorf.at>
2021-09-29 21:56:29 +0200

    progress

--- Exa_101_Kunden-Datenbank/src/main/java/at/kaindorf/json/JSONDeserializer.java
+++ Exa_101_Kunden-Datenbank/src/main/java/at/kaindorf/json/JSONDeserializer.java
@@ -4,14 +4,16 @@ import at.kaindorf.pojos.Customer;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class JSONDeserializer extends StdDeserializer<Customer> {
 
-    public static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd-MMM-yyyy", Locale.ENGLISH);
 
     public JSONDeserializer() {
         super(Customer.class);
@@ -19,7 +21,9 @@ public class JSONDeserializer extends StdDeserializer<Customer> {
 
     @Override
     public Customer deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-
-        return null;
+        JsonNode node = deserializationContext.readValue(jsonParser, JsonNode.class);
+        //TODO: Create Objects
+        Customer customer = new Customer();
+        return customer;
     }
 }
