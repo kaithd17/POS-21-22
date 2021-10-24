8ef3bd5e9eaa38f0d6812aba40f31af51c33d552
Thomas Kainz <kaithd17@htl-kaindorf.at>
2021-10-24 13:37:36 +0200

    progress

--- Exa_105_JPA_AirlineReservationSystem/src/main/java/at/kaindorf/airline/pojos/AirlinePK.java
+++ Exa_105_JPA_AirlineReservationSystem/src/main/java/at/kaindorf/airline/pojos/AirlinePK.java
@@ -3,22 +3,13 @@ package at.kaindorf.airline.pojos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@IdClass(AirlinePK.class)
-public class Airline implements Serializable {
-    @Id
-    @Column(name = "airline_id")
+public class AirlinePK implements Serializable {
     private Long airlineId;
-
-    @Id
-    @Column(name = "airline_name", length = 40)
     private String airlineName;
 }
--- Exa_105_JPA_AirlineReservationSystem/src/main/java/at/kaindorf/airline/pojos/Flight.java
+++ Exa_105_JPA_AirlineReservationSystem/src/main/java/at/kaindorf/airline/pojos/Flight.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.sql.Time;
 
 @Data
 @AllArgsConstructor
@@ -14,4 +17,30 @@ import javax.persistence.Id;
 public class Flight {
     @Id
     private Long flightId;
+
+    @ManyToOne
+    @Column(name = "aircraft_id")
+    private Aircraft aircraft;
+
+    @ManyToOne
+    @Column(name = "airline_id")
+    private Airline airlineId;
+
+    @ManyToOne
+    @Column(name = "airline_name")
+    private Airline airlineName;
+
+    @ManyToOne
+    @Column(name = "departure_airport")
+    private Airport departureAirport;
+
+    @Column(name = "departure_time")
+    private Time departureTime;
+
+    @ManyToOne
+    @Column(name = "arrival_airport")
+    private Airport arrivalAirport;
+
+    @Column(name = "arrival_time")
+    private Time arrivalTime;
 }
