ac90055ec974d65dc078f831f42b0c717c26c8bd
Thomas Kainz <kaithd17@htl-kaindorf.at>
2022-02-11 12:58:27 +0100

    done with userstory 2 + 3

--- Exa_104_SpringRest_ExamDB/src/main/java/at/kaindorf/exa_104_springrest_examdb/api/ExamController.java
+++ Exa_104_SpringRest_ExamDB/src/main/java/at/kaindorf/exa_104_springrest_examdb/api/ExamController.java
@@ -1,5 +1,34 @@
 package at.kaindorf.exa_104_springrest_examdb.api;
 
 
+import at.kaindorf.exa_104_springrest_examdb.database.ExamRepository;
+import at.kaindorf.exa_104_springrest_examdb.database.StudentRepository;
+import at.kaindorf.exa_104_springrest_examdb.pojos.Exam;
+import at.kaindorf.exa_104_springrest_examdb.pojos.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/exam")
 public class ExamController {
+
+    @Autowired
+    private ExamRepository examRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/getExams")
+    public ResponseEntity<List<Exam>> getExamsFromStudent(@RequestParam("studentId") Long studentId) {
+        Student student = studentRepository.findById(studentId).get();
+        List<Exam> exams = examRepository.findAllByStudent(student);
+        return ResponseEntity.of(Optional.of(exams));
+    }
 }
